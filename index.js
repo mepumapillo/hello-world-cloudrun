abb5a73b982cfcfe106acede49295da85dcbf054
mepumapillo <trainee@reechai.com>
2026-06-10 16:38:52 -0500

    fix: elimina linea con error de sintaxis (const roto) que impedia arrancar el contenedor

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ import functions from '@google-cloud/functions-framework';
 // Función HTTP. Sirve igual como Cloud Function (2nd gen) o Cloud Run.
 functions.http('helloWorld', (req, res) => {
   const nombre = req.query.nombre || req.body?.nombre || 'mundo';
-  const roto = ;   // ERROR DE SINTAXIS A PROPOSITO
   res.status(200).send(`Hola ${nombre} 👋 desde Cloud Run (CD activo ✅ v2)`);
 });
