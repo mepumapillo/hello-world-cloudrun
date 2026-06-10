b7e8da27e3351fc49df4d2412ddd0b513af54032
mepumapillo <trainee@reechai.com>
2026-06-10 16:27:08 -0500

    feat: mensaje v2 para probar despliegue continuo

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import functions from '@google-cloud/functions-framework';
 // Función HTTP. Sirve igual como Cloud Function (2nd gen) o Cloud Run.
 functions.http('helloWorld', (req, res) => {
   const nombre = req.query.nombre || req.body?.nombre || 'mundo';
-  res.status(200).send(`Hola ${nombre} 👋 desde Cloud Run`);
+  res.status(200).send(`Hola ${nombre} 👋 desde Cloud Run (CD activo ✅ v2)`);
 });
